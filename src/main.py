b8794e0c8b546e632fe3b296ccbe0fc2fc0857e9
Nguyen Hop <haohopnguyen@gmail.com>
2025-06-17 21:16:18 +0700

    Add: Train and Evaluate model by Logistics Regression

--- src/main.py
+++ src/main.py
@@ -42,6 +42,19 @@ config2 = [
         "Mô hình 4: Dự đoán theo category, num_lectures, content_length_min, avg_rating"
     )
 ]
+config3 = [
+    (
+        ["price", "subcategory", "language", "content_length_min"],
+        "models/model5",
+        "Mô hình 5: Dự đoán theo price, subcategory, content_length_min"
+    ),
+    (
+        ["price", "num_subscribers", "avg_rating", "category"],
+        "models/model6",
+        "Mô hình 6: Dự đoán theo price, num_subscribers, avg_rating, category"
+    )
+]
+
 
 # Chạy từng mô hình
 for features, model_path, model_name in config1:
@@ -51,6 +64,12 @@ for features, model_path, model_name in config1:
     evaluate_model(model, df, label_col=label, model_name=model_name, model_path=model_path)
 
 for features, model_path, model_name in config2:
+    print(f"\n===== Đang huấn luyện: {model_name} =====")
+    stages = build_feature_pipeline(df, features)
+    model = train_and_save(df, stages, label, model_path)
+    evaluate_model(model, df, label_col=label, model_name=model_name, model_path=model_path)
+
+for features, model_path, model_name in config3:
     print(f"\n===== Đang huấn luyện: {model_name} =====")
     stages = build_feature_pipeline(df, features)
     model = train_and_save(df, stages, label, model_path)
